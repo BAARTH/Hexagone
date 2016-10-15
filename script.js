e65929413c1c2da8282276c461ef7458b71254bb
JohannDesobry <Soulakwet@macbook-pro-de-johann-3.home>
2016-10-15 15:33:29 +0200

    samedi

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Yo');
+var hamb = document.querySelector('.hamburger');
+var hambBg = document.querySelector('#hamb-bg');
+
+hamb.onclick = function()
+{
+	if (hamb.classList.contains('is-active'))
+	{
+		hamb.classList.remove('is-active');
+		hambBg.style.width = '0px';
+		hambBg.style.right = '-400px';
+		hambBg.style.opacity = '0';
+	}
+	else
+	{
+		hamb.classList.add('is-active');
+		hambBg.classList.remove('displayNone');
+		hambBg.style.width = '400px';
+		hambBg.style.right = '0px';
+		hambBg.style.opacity = '1';
+	}
+}
